698ff5e915e89c0d9d9c86e360d31c267ac3f200
Jean-Baptiste Théou <jb@essential.com>
2019-05-13 07:46:21 +0900

    techpack: audio: tfa98xx: add error translation routine
    Change-Id: I4d1d428f0306bbe6bb25d8beab8a74307aaeadf7

--- techpack/audio/asoc/codecs/tfa9872/tfa98xx.c
+++ techpack/audio/asoc/codecs/tfa9872/tfa98xx.c
@@ -3430,13 +3430,13 @@ static void tfa98xx_interrupt(struct work_struct *work)
 			int no_clk = TFA_GET_BF(tfa98xx->handle, NOCLK);
 			/* Detect for clock is lost! (clock is not stable) */
 			if ((tfa98xx->handle == 0) && (no_clk == 1)) {
-				enum tfa98xx_error err;
+				enum tfa_error err;
 				/* Clock is lost. Set I2CR to remove POP noise */
 				pr_info("No clock detected. Resetting I2CR to avoid pop on 72!\n");
 				err = tfa98xx_tfa_start
 					(tfa98xx, tfa98xx_profile,
 					tfa98xx_vsteps);
-				if (err != TFA98XX_ERROR_OK)
+				if (err != tfa_error_ok)
 					pr_err("Error loading i2c registers (tfa_start), err=%d\n",
 						err);
 				else
