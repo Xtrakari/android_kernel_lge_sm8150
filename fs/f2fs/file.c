47f5b731bdd1350cffdaa84cb19b4bdccfe6487f
Richard Raya <rdxzv.dev@gmail.com>
2026-02-05 23:08:52 -0300

    Merge tag 'v4.14.356-openela' of https://github.com/openela/kernel-lts
    This is the 4.14.356 OpenELA-Extended LTS stable release
    
    * tag 'v4.14.356-openela' of https://github.com/openela/kernel-lts: (283 commits)
      LTS: Update to 4.14.356
      f2fs: correct removexattr behavior for null valued extended attribute
      mm: shmem: fix data-race in shmem_getattr()
      nilfs2: fix kernel bug due to missing clearing of checked flag
      ocfs2: pass u64 to ocfs2_truncate_inline maybe overflow
      nilfs2: fix potential deadlock with newly created symlinks
      wifi: iwlegacy: Clear stale interrupts before resuming device
      wifi: mac80211: do not pass a stopped vif to the driver in .get_txpower
      Revert "driver core: Fix uevent_show() vs driver detach race"
      xhci: Fix Link TRB DMA in command ring stopped completion event
      usb: phy: Fix API devm_usb_put_phy() can not release the phy
      misc: sgi-gru: Don't disable preemption in GRU driver
      net: amd: mvme147: Fix probe banner message
      netfilter: nft_payload: sanitize offset and length before calling skb_checksum()
      net: skip offload for NETIF_F_IPV6_CSUM if ipv6 header contains extension
      net: support ip generic csum processing in skb_csum_hwoffload_help
      net/sched: stop qdisc_tree_reduce_backlog on TC_H_ROOT
      gtp: allow -1 to be specified as file description from userspace
      gtp: simplify error handling code in 'gtp_encap_enable()'
      wifi: mac80211: skip non-uploaded keys in ieee80211_iter_keys
      ...
    
    Change-Id: Ief716c49d9d3d82907aa601504e2b5e002eee6f8
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -2031,6 +2031,9 @@ static int f2fs_ioc_start_atomic_write(struct file *filp)
 	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
 	int ret;
 
+	if (!(filp->f_mode & FMODE_WRITE))
+		return -EBADF;
+
 	if (!inode_owner_or_capable(inode))
 		return -EACCES;
 
@@ -2097,6 +2100,9 @@ static int f2fs_ioc_commit_atomic_write(struct file *filp)
 {
 	struct inode *inode = file_inode(filp);
 	int ret;
+
+	if (!(filp->f_mode & FMODE_WRITE))
+		return -EBADF;
 
 	if (!inode_owner_or_capable(inode))
 		return -EACCES;
@@ -2177,6 +2183,9 @@ static int f2fs_ioc_release_volatile_write(struct file *filp)
 {
 	struct inode *inode = file_inode(filp);
 	int ret;
+
+	if (!(filp->f_mode & FMODE_WRITE))
+		return -EBADF;
 
 	if (!inode_owner_or_capable(inode))
 		return -EACCES;
